d29336e4ab14dbb923e6f51ccbc115efe2e14341
jdornemann <johann.dornemann.c@gmail.com>
2018-10-02 21:44:56 -0300

    Ejercicios

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,17 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+
+
+ resultado = ''
+4.times do |x|
+    x+=1
+    4.times do |y|
+        y+= 1
+       resultado +=  (y*x).to_s + ' '
+    end
+
+    puts resultado
+    resultado = ''
+
+end
